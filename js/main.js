987c2e4e271e0a663f137f85dc416cb520f965d8
Paul Padian <paulpadian@Pauls-MacBook-Pro.local>
2020-06-03 16:12:17 -0600

    working kanye

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 console.log("👀")
+
+/ fetch('https://api.kanye.rest')
+    .then(function(responseData) {
+        let jsonData = responseData.json()
+        return jsonData;
+    })
+    .then(function(jsonData){
+        //do stuff
+        let quote = jsonData.quote;
+        console.log("Love that Data")
+        let kanyeQuote = document.createElement('li');            
+        kanyeQuote.textContent = quote;
+        document.getElementById('quote').appendChild(kanyeQuote);
+       
+    })
+    .catch(function(error) {
+        console.log('poo')
+        console.log(error)
+    })
+
+ 
